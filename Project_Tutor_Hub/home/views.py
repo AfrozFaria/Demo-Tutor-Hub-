78acd479efa2d4c19fdc8c36f1fa587dca25c76e
B. M. Arifuzzaman <bm.arifuzzaman@northsouth.edu>
2021-05-05 16:04:04 +0600

    test merge

--- Project_Tutor_Hub/home/views.py
+++ Project_Tutor_Hub/home/views.py
@@ -92,7 +92,6 @@ def signout(request):
     return redirect('landing')
 
 
-
 @login_required
 def profile(request):
     '''
@@ -104,12 +103,6 @@ def profile(request):
 
 
 def edit_profile_tutor(request):
-    '''
-    This will redirect the url to update the profile if the user is tutor where they
-    can update their necessary informations which will be displayed on profile page after saving them
-    :type request: HttpResponse
-    :param request: Takes the request to show edit_profile_tutor.html
-    '''
 	tutor= request.user.tutor
 	form = EditForm_Tutor(instance=tutor)
     
@@ -125,12 +118,6 @@ def edit_profile_tutor(request):
 
 
 def edit_profile_student(request):
-     '''
-    This will redirect the url to update the profile if the user is student where they
-    can update their necessary informations which will be displayed on profile page after saving them
-    :type request: HttpResponse
-    :param request: Takes the request to show edit_profile_student.html
-    '''
 	student= request.user.student
 	form = EditForm_Student(instance=student)
 	if request.method == 'POST':
